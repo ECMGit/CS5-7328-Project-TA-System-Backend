78890171239b9647547af7bc178f58405a164d0c
SMJD11 <78318583+SMJD11@users.noreply.github.com>
2023-10-20 16:02:32 -0500

    Add files via upload

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import userRoutes from './modules/user/user.routes';
+import * as UserController from './modules/user/user.controller';
+import taApplicationRoutes from './modules/user/taApplication.routes';
+import taJobRoutes from './modules/user/taJobs.routes';
+
 
 const app = express();
 
@@ -26,8 +30,8 @@ app.use((req, res, next) => {
 
 // import routes which are defined in modules
 app.use('/user', userRoutes);
-
-
+app.use('/taApplication', taApplicationRoutes);
+app.use('/taJob', taJobRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
