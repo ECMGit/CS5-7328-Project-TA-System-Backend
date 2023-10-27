a2bb7e8e68f4700c9aec2d2785eb409ce1cf8092
blake-gebhardt <blakewgebhardt@gmail.com>
2023-10-27 16:25:32 -0500

    reworking with the react router at the end

--- src/routes/passwordResetRouter.ts
+++ src/routes/passwordResetRouter.ts
@@ -3,43 +3,44 @@ import nodemailer from 'nodemailer';
 import crypto from 'crypto';
 import { prisma } from '../../prisma';
 import bcrypt from 'bcrypt';
+
 const router = express.Router();
 
+// Handle the request for sending a password reset link
 router.post('/password-reset-link', async (req, res) => {
-
   const { email } = req.body;
   console.log(email);
-  // 1. Verify if email is in database
+
+  // 1. Verify if the email is in the database
   const user = await prisma.user.findUnique({
     where: {
       email: email
     }
   });
 
+  // If the email is not found in the database, return an error response
   if (!user) {
     return res.status(400).send('Email not found');
   }
 
+  // Generate a unique token and create a reset link
   const timestamp = Date.now();
   const currentDate = new Date(timestamp);
-
   const token = crypto.randomBytes(20).toString('hex');
-
   const resetLink = process.env.FRONTEND_URL + `resetPassword/${token}`;
 
-  // Save token to database
+  // Save the token and its expiry time to the database
   await prisma.user.update({
     where: {
       email: user.email
     },
     data: {
       resetToken: token,
-      resetTokenExpiry: Date.now() + 3600000 // 1 hour expiry
+      resetTokenExpiry: Date.now() + 3600000 // 1-hour expiry
     }
   });
 
-  console.log(process.env.EMAIL_USER);
-  // Create a transporter object
+  // Create a transporter object for sending emails
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -48,7 +49,7 @@ router.post('/password-reset-link', async (req, res) => {
     }
   });
 
-  // Email content
+  // Compose the email content
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: email,
@@ -57,39 +58,41 @@ router.post('/password-reset-link', async (req, res) => {
   };
 
   try {
+    // Send the reset email and respond with a success message
     await transporter.sendMail(mailOptions);
     res.status(200).send({ message: 'Reset email sent' });
   } catch (error) {
+    // If there's an error sending the email, return an error response
     console.error(error);
     res.status(500).send('Error sending email');
   }
-
 });
 
+// Handle the request for confirming and updating a password reset
 router.post('/password-reset/confirm', async (req, res) => {
-
   const { token, password } = req.body;
 
-  // 1. Find user by token
+  // 1. Find the user with the provided token
   const user = await prisma.user.findUnique({
     where: {
       resetToken: token
     }
   });
 
+  // If the user with the token is not found, return an error response
   if (!user) {
     return res.status(400).send('Invalid token');
   }
 
-  // 2. Check if token expired
+  // 2. Check if the token has expired
   if (user.resetTokenExpiry && Date.now() > user.resetTokenExpiry) {
     return res.status(400).send('Token expired');
   }
 
-  // 3. Hash new password
+  // 3. Hash the new password
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  // 4. Update user password
+  // 4. Update the user's password and clear the reset token
   await prisma.user.update({
     where: { email: user.email },
     data: {
@@ -99,9 +102,8 @@ router.post('/password-reset/confirm', async (req, res) => {
     }
   });
 
-  // 5. Send response
+  // 5. Send a success response
   res.send({ message: 'Password updated successfully' });
-
 });
 
 export default router;
