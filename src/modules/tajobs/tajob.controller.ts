433bc660fbd60b055766a4822530399a24c496dd
snehagalex <71729811+snehagalex@users.noreply.github.com>
2023-10-27 15:44:40 -0500

    Update tajob.controller.ts
    added comments to tajob.controller.ts

--- src/modules/tajobs/tajob.controller.ts
+++ src/modules/tajobs/tajob.controller.ts
@@ -10,6 +10,8 @@ import { Request, Response, NextFunction } from 'express';
  * @param next 
  * @returns 
  */
+
+//this get function returns all available TA jobs
 export const getAllTAJobs = async (
   req: Request,
   res: Response,
@@ -18,9 +20,11 @@ export const getAllTAJobs = async (
   console.log('in get all');
 
   try {
+    // return all jobs that have been published
     const taJobs = await UserService.getAllTAJobs();
     if (taJobs.length == 0) {
-      return res.status(404).json({ message: 'Np job listings found.' });
+      // if there are no jobs found, return message that no jobs are found
+      return res.status(404).json({ message: 'No job listings found.' });
     }
     res.json(taJobs);
   } catch (error) {
@@ -29,14 +33,17 @@ export const getAllTAJobs = async (
   }
 };
 
+// this GET function searches for a TA job by job ID that the user passes in
 export const getTAJobById = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
+    // return jobs with matching job ID
     const taJob = await UserService.getTAJobById(Number(req.params.id));
     if (!taJob) {
+      // if no TA job is found with the required ID, return message indicating issue
       return res.status(404).json({ message: 'TA job not found' });
     }
     res.json(taJob);
@@ -78,10 +85,13 @@ export const getTAJobsWithFilters = async (
  * @param next 
  * @returns 
  */
+//this function returns a list of jobs posted by the faculty member with the passed-in ID
 export const getTAJobsByFacultyId = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    // return jobs with matching faculty ID
       const user = await UserService.getTAJobsByFacultyId(Number(req.params.facultyId));
       if (!user) {
+        // if there are no jobs posted by the faculty member or if the faculty is not found, return this message
           return res.status(404).json({ message: 'User not found' });
       }
       res.json(user);
