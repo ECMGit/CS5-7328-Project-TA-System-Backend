47374cce3e4c637ba8ac87a816a492aa85939f4f
Miller <Millerjboyd@gmail.com>
2023-12-03 22:56:44 -0600

    put token back

--- src/app.ts
+++ src/app.ts
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 app.use('/user', userRoutes);
 // routes that require middleware
 app.use('/ta-application', verifyToken, taApplicationRoutes);
-app.use('/jobs', tajobRoutes);
+app.use('/jobs', verifyToken, tajobRoutes);
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
