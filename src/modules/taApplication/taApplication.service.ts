07c0755d85de188e01f5779a2ed5e77f2e9a5631
mingyao1 <106688909+mingyao1@users.noreply.github.com>
2023-12-08 23:22:35 -0600

    Add message on application submission
    Test cases are not included for this because it is primarily dealing with the database.

--- src/modules/taApplication/taApplication.service.ts
+++ src/modules/taApplication/taApplication.service.ts
@@ -8,26 +8,90 @@ import { prisma } from '../../../prisma';
  * @param data  Ta application data
  * @param file  resume file
  */
-export const saveApplication =
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    async (data: TAApplicationData, file: Express.Multer.File)
-        : Promise<TAApplication | null> => {
-      const filePath = file.path;
-      return await prisma.tAApplication.create({
+
+async function createMessagesForFaculty(applicationData: TAApplicationData) {
+  // Retrieve the course faculties
+  const faculties = await prisma.facultyCourse.findMany({
+    where: {
+      courseId: applicationData.courseId,
+    },
+    include: {
+      faculty: {
+        include: {
+          user: true, // To get facultyName
+        },
+      },
+    },
+  });
+
+  // Retrieve the student's name and TA job title
+  const student = await prisma.student.findUnique({
+    where: {
+      userId: applicationData.studentId,
+    },
+    include: {
+      user: true,
+    },
+  });
+
+  const taJob = await prisma.tAJob.findUnique({
+    where: {
+      id: applicationData.taJobId,
+    },
+  });
+
+  if (!student || !student.user || !taJob) {
+    throw new Error('Student or TA Job not found');
+  }
+
+  const studentName = `${student.user.firstName} ${student.user.lastName}`;
+  const taJobTitle = taJob.title;
+
+  // Create messages for each faculty member
+  await Promise.all(
+    faculties.map(async (fc) => {
+      const facultyName = `${fc.faculty.user.firstName} ${fc.faculty.user.lastName}`;
+      const personalizedMessage = `This is an automated message.\nDear ${facultyName}, ${studentName} just submitted an application for ${taJobTitle}. View it here.`;
+
+      await prisma.userMessage.create({
         data: {
-          course: { connect: { id: data.courseId } },
-          student: { connect: { userId: data.studentId } },
-          taJob: { connect: { id: data.taJobId } },
-          hoursCanWorkPerWeek: data.hoursCanWorkPerWeek,
-          // eslint-disable-next-line @typescript-eslint/naming-convention
-          GPA: data.gpa,
-          requiredCourses: data.requiredCourses,
-          requiredSkills: data.requiredSkills,
-          resumeFile: filePath,
-          coursesTaken: data.coursesTaken,
+          content: personalizedMessage,
+          senderId: student.userId,
+          receiverId: fc.faculty.userId,
+          applicationId: applicationData.taJobId, // Assuming this is the correct field to link the message to the application
         },
       });
-    };
+    })
+  );
+}
+
+export const saveApplication =
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  async (data: TAApplicationData, file: Express.Multer.File)
+    : Promise<TAApplication | null> => {
+    const filePath = file.path;
+    try {
+      createMessagesForFaculty(data);
+    }
+    catch (error) {
+      // This error should not prevent application submission
+      console.log(error);
+    }
+    return await prisma.tAApplication.create({
+      data: {
+        course: { connect: { id: data.courseId } },
+        student: { connect: { userId: data.studentId } },
+        taJob: { connect: { id: data.taJobId } },
+        hoursCanWorkPerWeek: data.hoursCanWorkPerWeek,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        GPA: data.gpa,
+        requiredCourses: data.requiredCourses,
+        requiredSkills: data.requiredSkills,
+        resumeFile: filePath,
+        coursesTaken: data.coursesTaken,
+      },
+    });
+  };
 
 /**
  * Get a application by id
