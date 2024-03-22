d8ec8c4fb370dde0d861f23c428a437b4094b1c2
Drew Harris <harrisd@smu.edu>
2024-03-22 12:05:24 -0500

    Fix eslint and comment code

--- src/modules/feedback/index.ts
+++ src/modules/feedback/index.ts
@@ -1,32 +1,34 @@
-import { Router } from "express";
-import { prisma } from "prisma/index";
+import { Router } from 'express';
+import { prisma } from 'prisma/index';
 
 const feedbackRouter = Router();
 
-feedbackRouter.post("/", (req, res) => {
-    prisma.feedback.create({
-        data: {
-            content: req.body.content,
-            complete: false,
-            leftBy: {
-                connect: {
-                    id: req.body.userId
-                }
-            }
-        }
-    }).then((feedback) => {
-        res.status(200).json(feedback);
-    }).catch((err) => {
-        res.status(500).json(err);
+// Route to create a new piece of feedback
+feedbackRouter.post('/', (req, res) => {
+  prisma.feedback
+    .create({
+      data: {
+        content: req.body.content,
+        complete: false,
+        leftBy: {
+          connect: {
+            id: req.body.userId,
+          },
+        },
+      },
     })
+    .then((feedback) => {
+      res.status(200).json(feedback);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
-feedbackRouter.get("/", async(req, res) => {
-
-    return await prisma.feedback.findMany();
-
+// Get all of the feedback to show to the student
+feedbackRouter.get('/', async (req, res) => {
+  return res.json(await prisma.feedback.findMany());
 });
 
+export { feedbackRouter };
 
-
-export { feedbackRouter }
