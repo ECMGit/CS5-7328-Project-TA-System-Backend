0d981717fca918304c847ec5ca1cf7751cd78a23
PingHe87 <heping.m87@gmail.com>
2024-04-07 16:35:22 -0500

    add functions to make sure faculty can only see applications of their own courses

--- src/modules/taApplication/taApplication.routes.ts
+++ src/modules/taApplication/taApplication.routes.ts
@@ -3,10 +3,12 @@ import * as TaApplicationController from './taApplication.controller';
 
 const router = Router();
 
-router.post( '/', TaApplicationController.save );
+router.post('/', TaApplicationController.save);
 router.get('/', TaApplicationController.getTaApplications); // route is not protected by authentication middleware
 router.get('/:id', TaApplicationController.getTaApplication);
-router.get('/student/:studentId', TaApplicationController.getTaApplicationByStudentId);
+router.get('/faculty/:facultyId', TaApplicationController.getTaApplicationsByFacultyId);
+//faculty only see applications of their courses
+router.get('/student/:studentId', TaApplicationController.getTaApplicationsByStudentId);
 router.get('/:courseId', TaApplicationController.getTaApplicationByCourseId);
 router.post('/:id', TaApplicationController.updateTaApplication);
 router.delete('/:id', TaApplicationController.deleteTaApplication);
