aea0aed529e5658b4a1b9a92897492ca278f46f7
Drew Harris <harrisd@smu.edu>
2024-04-10 11:13:56 -0500

    Singlequote strings in app.ts
    Please fix prettier omg istg

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express from "express";
-import bodyParser from "body-parser";
-import userRoutes from "./modules/user/user.routes";
-import taApplicationRoutes from "./modules/taApplication/taApplication.routes";
-import tajobRoutes from "./modules/tajobs/tajob.routes";
-import messageRoutes from "./modules/message/message.routes";
-import taPerformanceRoute from "./modules/taPerformance/taPerformance.routes";
-import courseRoutes from "./modules/course/course.routes";
-import taskRoutes from "./modules/tasks/tasks.routes";
+import express from 'express';
+import bodyParser from 'body-parser';
+import userRoutes from './modules/user/user.routes';
+import taApplicationRoutes from './modules/taApplication/taApplication.routes';
+import tajobRoutes from './modules/tajobs/tajob.routes';
+import messageRoutes from './modules/message/message.routes';
+import taPerformanceRoute from './modules/taPerformance/taPerformance.routes';
+import courseRoutes from './modules/course/course.routes';
+import taskRoutes from './modules/tasks/tasks.routes';
 
 // middleware
-import { verifyToken } from "./middleware/authentication";
-import { feedbackRouter } from "./modules/feedback";
+import { verifyToken } from './middleware/authentication';
+import { feedbackRouter } from './modules/feedback';
 
 const app = express();
 
@@ -23,38 +23,38 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, DELETE"
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, DELETE'
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   // Handle preflight request.
   // By default, send a 200 status for OPTIONS requests.
-  if (req.method === "OPTIONS") {
+  if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
 
   // api log
-  console.log("Incoming request:", req.method, req.originalUrl, req.query);
+  console.log('Incoming request:', req.method, req.originalUrl, req.query);
 
   next();
 });
 
 // import routes which are defined in modules
-app.use("/user", userRoutes);
+app.use('/user', userRoutes);
 // routes that require middleware
-app.use("/message", verifyToken, messageRoutes);
-app.use("/ta-application", verifyToken, taApplicationRoutes);
-app.use("/jobs", verifyToken, tajobRoutes);
-app.use("/api/ta-performance", taPerformanceRoute);
-app.use("/course", verifyToken, courseRoutes);
-app.use("/tasks", verifyToken, taskRoutes);
-app.use("/feedback", feedbackRouter);
+app.use('/message', verifyToken, messageRoutes);
+app.use('/ta-application', verifyToken, taApplicationRoutes);
+app.use('/jobs', verifyToken, tajobRoutes);
+app.use('/api/ta-performance', taPerformanceRoute);
+app.use('/course', verifyToken, courseRoutes);
+app.use('/tasks', verifyToken, taskRoutes);
+app.use('/feedback', feedbackRouter);
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!');
 });
 
 export default app;
